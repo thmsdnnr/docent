656cd4b7e811b9dbdb089290163879c9ef612ca7
Thomas Danner <thmsdnnr@gmail.com>
2019-06-04 18:18:33 -0700

    sweet, we can flip the card and it looks decent.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
+import 'package:flutter/rendering.dart';
+
 void main() => runApp(MyApp());
+
+enum FlashCardSide { front, back }
 
 class FlashCard {
   FlashCard(this.id, this.title, this.front, this.back, this.tags, this.decks);
@@ -28,8 +32,36 @@ class FlashCard {
   }
 
   Map toJson() {
-    return {"id": id, "title": title, "front": front, "back": back, "tags": tags.toString(), "decks": decks.toString()};
+    return {
+      "id": id,
+      "title": title,
+      "front": front,
+      "back": back,
+      "tags": tags.toString(),
+      "decks": decks.toString()
+    };
   }
+
+  FractionallySizedBox toWidget({FlashCardSide sideToDisplay}) =>
+      FractionallySizedBox(
+        widthFactor: 0.8,
+        heightFactor: 0.8,
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(
+                title: Text(
+                  sideToDisplay == FlashCardSide.front ? front : back,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 32),
+                  textAlign: TextAlign.center,
+                ),
+              )
+            ],
+          ),
+        ),
+      );
 }
 
 class MyApp extends StatelessWidget {
@@ -39,15 +71,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -57,16 +80,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -74,73 +87,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    String theJSON = """{"id":"a globally unique ID to identify the card","title":"an optional title","front":
-      "This is the front of the card!","back":"This is the back of the card!","tags":["a","list","of","tags",
-      "that","the","card","has"],"decks":["a","list","of","globally unique","deck IDs","that","this","Card",
-      "Belongs","to"]}""";
-    FlashCard card = new FlashCard.fromJson(theJSON);
-    print(card.toString());
+  FlashCardSide _shownSide = FlashCardSide.front;
+  FractionallySizedBox _cardToShow;
+  static final Icon flipToBack = new Icon(Icons.flip_to_back);
+  static final Icon flipToFront = new Icon(Icons.flip_to_front);
+  Icon _thisIcon = flipToBack;
+
+  static String theJSON =
+      """{"id":"a globally unique ID to identify the card","title":"an optional title","front":
+  "This is the front of the card!","back":"This is the back of the card!","tags":["a","list","of","tags",
+  "that","the","card","has"],"decks":["a","list","of","globally unique","deck IDs","that","this","Card",
+  "Belongs","to"]}""";
+  FlashCard _card = new FlashCard.fromJson(theJSON);
+
+  @override
+  initState() {
+    super.initState();
+    _cardToShow = _card.toWidget(sideToDisplay: _shownSide);
+  }
+
+  void _flipCard() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_shownSide == FlashCardSide.front) {
+        _shownSide = FlashCardSide.back;
+        _cardToShow = _card.toWidget(sideToDisplay: _shownSide);
+        _thisIcon = flipToBack;
+      } else {
+        _shownSide = FlashCardSide.front;
+        _cardToShow = _card.toWidget(sideToDisplay: _shownSide);
+        _thisIcon = flipToFront;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: new Center(
+        child: _cardToShow,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _flipCard,
+        tooltip: 'Flip Card',
+        child: _thisIcon,
+      ),
     );
   }
 }
