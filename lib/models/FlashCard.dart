b7b217b56ef006e0d2ed447a205547adc3bd044b
Thomas Danner <thmsdnnr@gmail.com>
2019-06-09 16:43:21 -0700

    simplify Models substantially

--- lib/models/FlashCard.dart
+++ lib/models/FlashCard.dart
@@ -1,51 +1,32 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 
 enum FlashCardSide { front, back }
 
 class FlashCard {
-  FlashCard(this.id, this.title, this.front, this.back, this.tags, this.decks);
+  FlashCard({this.id, this.title, this.front, this.back});
 
-  String id;
+  int id;
   String title;
   String front;
   String back;
-  List<String> tags;
-  List<String> decks;
 
   String toString() {
-    return "$id $title $front $back ${tags.toString()}, ${decks.toString()}";
+    return "$id $title $front $back";
   }
 
-  FlashCard.fromJsonString(String jsonString) {
-    final Map<String, dynamic> jsonData = json.decode(jsonString);
-    id = jsonData["id"];
-    title = jsonData["title"];
-    front = jsonData["front"];
-    back = jsonData["back"];
-    tags = List.from(jsonData["tags"]);
-    decks = List.from(jsonData["decks"]);
-  }
+  factory FlashCard.fromMap(Map<String, dynamic> json) => new FlashCard(
+        id: json["id"],
+        title: json["title"],
+        front: json["front"],
+        back: json["back"],
+      );
 
-  FlashCard.fromObject(flashcardObj) {
-    id = flashcardObj["id"];
-    title = flashcardObj["title"];
-    front = flashcardObj["front"];
-    back = flashcardObj["back"];
-    tags = List.from(flashcardObj["tags"]);
-    decks = List.from(flashcardObj["decks"]);
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      "id": id,
-      "title": title,
-      "front": front,
-      "back": back,
-      "tags": tags.toString(),
-      "decks": decks.toString()
-    };
-  }
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "front": front,
+        "back": back,
+      };
 
   FractionallySizedBox toWidget({FlashCardSide sideToDisplay}) =>
       FractionallySizedBox(
