fd443f6757c0de78c45452bf9fac2deec078c50f
Thomas Danner <thmsdnnr@gmail.com>
2019-06-10 22:06:18 -0700

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class _MyHomePageState extends State<MyHomePage> {
   int _totalCardCt = 0;
   int _cardGrade = -1;
   Deck _chosenDeck;
-  Card _chosenCard;
   PageController controller;
 
   static final FlashCardSide BackSideOfCard = FlashCardSide.back;
@@ -58,24 +57,6 @@ class _MyHomePageState extends State<MyHomePage> {
   initState() {
     super.initState();
     grabCardsAndDisplay();
-  }
-
-  Future<void> buildFakeData() async {
-    Deck newDeck = new Deck(id: 6, title: "A sick deck");
-    Deck newDeck2 = new Deck(id: 4, title: "A sick deck");
-    await DBProvider.db.insertDeck(newDeck);
-    await DBProvider.db.insertDeck(newDeck2);
-    var futures = <Future>[];
-    new List<int>.generate(10, (i) {
-      FlashCard thisCard = new FlashCard(
-          id: i, title: "test card $i", front: "$i front", back: "$i back");
-      futures.add(DBProvider.db.insertFlashCard(thisCard));
-      futures.add(
-          DBProvider.db.insertDeckToFlashCard(deck: newDeck, card: thisCard));
-      futures.add(
-          DBProvider.db.insertDeckToFlashCard(deck: newDeck, card: thisCard));
-    });
-    await Future.wait(futures);
   }
 
   void grabCardsAndDisplay({int deckId, DeckPosition startAt}) async {
@@ -100,29 +81,6 @@ class _MyHomePageState extends State<MyHomePage> {
       } else {
         _shownSide = FlashCardSide.front;
       }
-    });
-  }
-
-  bool _canGoBack() => _activeCardIdx > 0 ? true : false;
-  bool _canGoForward() => _activeCardIdx == _totalCardCt - 1 ? false : true;
-
-  void _handleBackPress() {
-    if (!_canGoBack()) {
-      return;
-    }
-    setState(() {
-      _activeCardIdx = _activeCardIdx - 1;
-      _shownSide = FlashCardSide.front;
-    });
-  }
-
-  void _handleForwardPress() {
-    if (!_canGoForward()) {
-      return;
-    }
-    setState(() {
-      _activeCardIdx = _activeCardIdx + 1;
-      _shownSide = FlashCardSide.front;
     });
   }
 
@@ -251,32 +209,6 @@ class _MyHomePageState extends State<MyHomePage> {
       setState(() {
         grabCardsAndDisplay(deckId: createdDeckId["deckId"]);
       });
-    }
-  }
-
-  Widget buildCardNavigation() {
-    if (_cardList != null &&
-        _activeCardIdx >= 0 &&
-        _activeCardIdx < _cardList.length) {
-      return ButtonBar(
-        mainAxisSize: MainAxisSize.min,
-        alignment: MainAxisAlignment.center,
-        children: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.arrow_back, size: 32),
-            onPressed: _canGoBack() == true ? _handleBackPress : null,
-          ),
-          Text("${_activeCardIdx + 1} / $_totalCardCt",
-              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
-              textAlign: TextAlign.center),
-          new IconButton(
-            icon: new Icon(Icons.arrow_forward, size: 32),
-            onPressed: _canGoForward() == true ? _handleForwardPress : null,
-          ),
-        ],
-      );
-    } else {
-      return null;
     }
   }
 
--- lib/models/FlashCard.dart
+++ lib/models/FlashCard.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 enum FlashCardSide { front, back }
 
 class FlashCard {
